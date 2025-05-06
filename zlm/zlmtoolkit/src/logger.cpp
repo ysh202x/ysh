091afed4ce84c0c15824a11b443120ea8920e5e1
yushuhang <yushuhang@insta360.com>
2025-05-06 19:48:15 +0800

    logger

--- zlm/zlmtoolkit/src/logger.cpp
+++ zlm/zlmtoolkit/src/logger.cpp
@@ -16,3 +16,39 @@ Logger::Logger(const std::string &loggername)
     _logger_name = loggername;
 }
 
+Logger::~Logger()
+{
+    std::cout << "Logger destructor" << std::endl;
+}
+
+
+/*-----------------log_channel------------------- */
+log_channel::log_channel(const std::string &channel_name,LogLevel level)
+{
+    _channel_name = channel_name;
+    _level = level;
+}
+
+log_channel::~log_channel()
+{
+    std::cout << "log_channel destructor" << std::endl;
+}
+
+std::string log_channel::print_time(const timeval & tv)
+{
+    auto tm = localtime(&tv.tv_sec);
+    char buf[64] = {0};
+    snprintf(buf, sizeof(buf), "%d-%02d-%02d %02d:%02d:%02d.%03d",
+            1900 + tm.tm_year,
+            1 + tm.tm_mon,
+            tm.tm_mday,
+            tm.tm_hour,
+            tm.tm_min,
+            tm.tm_sec,
+            (int)(tv.tv_usec / 1000));
+
+    return buf;
+    
+}
+
+void 
--- zlm/zlmtoolkit/src/logger.h
+++ zlm/zlmtoolkit/src/logger.h
@@ -1,7 +1,14 @@
 #ifndef __UTIL_LOGGER_H__
 #define __UTIL_LOGGER_H__
 
-
+typedef enum
+{
+    LTrace,
+    LDebug,
+    LInfo,
+    LWarn,
+    LError,
+}LogLevel;
 
 
 class Logger
@@ -17,6 +24,19 @@ public:
 
 private:
     std::string _logger_name;
+}
+
+
+class log_channel
+{
+public:
+    log_channel(const std::string &channel_name,LogLevel level = LInfo);
+    virtual ~log_channel();
+
+//保护继承 可以在派生类使用    
+protected:
+    std::string _channel_name;
+    LogLevel _level;
 }
 
 
